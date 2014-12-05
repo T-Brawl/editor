4a5a40f71755d1a54abd51c75404a6ff76a06617
Levrifon <rems-msn@hotmail.fr>
2014-12-05 17:44:28 +0100

    a little improvement, Alex don't hesitate to read what I made in PluginFilter because it's very complicated, and there are a lot of exceptions

--- src/plugins/Main.java
+++ src/plugins/Main.java
@@ -3,20 +3,17 @@ package plugins;
 import java.io.File;
 
 public class Main {
-
-
-	
 	public static void main(String[] args) throws ClassNotFoundException { 
 		PluginFilter pointClass = new PluginFilter();
 		FileDetector mrString = new FileDetector();
-		PluginFinder checkStuff = new PluginFinder(new File("/home/l3/debue/workspace/Editor/bin/plugins"),pointClass);
+		PluginFinder checkStuff = new PluginFinder(new File("/home/rems/workspace/Editor/bin/plugins/"),pointClass);
 		
 		checkStuff.addFileListener(mrString);
 		String s = "ToUpperCase.class";
-		String[] newClass = new String[2];
+		/*String[] newClass = new String[2];
 		newClass = s.split(".");
-		Class<?> plugins = Class.forName("plugins.ToUpperCase");
-		try {
+		Class<?> plugins = Class.forName("plugins.ToUpperCase");*/
+		/*try {
 			plugins.newInstance();
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
@@ -24,9 +21,9 @@ public class Main {
 		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 
-		System.out.println(plugins.getInterfaces()[0].getClass());
+		/*System.out.println(plugins.getInterfaces()[0].getClass());*/
 		while(true);
 	}
 
--- src/plugins/PluginFilter.java
+++ src/plugins/PluginFilter.java
@@ -2,21 +2,48 @@ package plugins;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.lang.reflect.Constructor;
 
 public class PluginFilter implements FilenameFilter {
 
 	private String extension;
-	
+	private ClassLoader generatedClass;
+
 	public PluginFilter() {
 		super();
 		this.extension = ".class";
 	}
-	
+
 	@Override
 	public boolean accept(File directory, String file) {
+		String[] nameFile = new String[file.length()];
+		String name;
+		Constructor constructor;
+		System.out.println(file);
+		nameFile = file.split("\\.", file.length());
+		name = nameFile[0];
+		System.out.println(nameFile[0]);
+		try {
+			/* on recupere la class depuis le nameFile (le nom sans le .class) */
+			generatedClass = Class.forName("plugins." + name).getClassLoader();
+			System.out.println(generatedClass.getClass().getName());
+			/* on recupere le constructeur */
+			constructor = generatedClass.getClass().getConstructor();
+			/* si le constructeur ne prend pas de parametre */
+			if (constructor.getParameterTypes().length == 0) {
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
 		return file.endsWith(this.extension);
+
 	}
-	
+
 	public boolean accept(File name) {
 		return name.getName().endsWith(this.extension);
 	}
